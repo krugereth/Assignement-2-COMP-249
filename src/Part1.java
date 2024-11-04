4b5e1dbd6918ca311c0116834fb1c57d34a0e660
krugereth <86798072+krugereth@users.noreply.github.com>
2024-11-03 20:18:00 -0500

    Update Part1.java

--- src/Part1.java
+++ src/Part1.java
@@ -20,7 +20,7 @@ public class Part1 {
                     while ((line = fileReader.readLine()) != null) {
 
                         String[] fields = line.split(",");
-                        String[] fields2 = line.split("");
+                        String[] fields2 = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
                         if(fields2.length <= 1 && fields.length!=6){
                             BufferedWriter bw1 = new BufferedWriter(new FileWriter("syntax_error_file.txt",true));
                             bw1.write(line + "\n");
