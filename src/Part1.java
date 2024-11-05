a873aac49184f9e940af8d1cd9d4ec7aa4462b86
krugereth <kruether@Protonmail.com>
2024-11-05 13:34:32 -0500

    commit test 2

--- src/Part1.java
+++ src/Part1.java
@@ -1,20 +1,48 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Part1 {
 
     public static void do_part1() {
+
+       Scanner fileReader = null;
+       BufferedWriter fileWriter = null;
         // Opens the part1 input file names files to try and read the names of the files inside
-        try (BufferedReader br = new BufferedReader(new FileReader("part1_input_file_names.txt"))) {
+        try {
+            fileReader = new Scanner(new FileInputStream("part1_input_file_names.txt"));
+            fileWriter = new BufferedWriter(new FileWriter("syntax_error_file.txt", true));
+
+        }catch (FileNotFoundException e) {
+            System.out.println("File not found. The program will now terminate");
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("IO Exception");
+        }
+            int fileCount = 0;
+            int numFiles = fileReader.nextInt();
+
+        int invalidGenreCount = 0, tooManyFieldsCount = 0, tooFewFieldsCount = 0,  missingFieldCount = 0;
+        int CCBcount = 0, HCBcount = 0, MTVcount = 0, MRBcount = 0, NEBcount = 0, OTRcount = 0, SSMcount = 0, TPAcount = 0;
+
+        String title, authors, price, isbn, genre, year;
 
             String fileName; //String for the file names
 
             // Loop through each file name in the list
-            while ((fileName = br.readLine()) != null) {
+            while ((fileName = fileReader.nextLine()) != null) {
 
                 System.out.println("Processing file: " + fileName);
 
+                Scanner bookRecordScanner = null;
                 // Try to open and read the contents of each file
-                try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+                try {
+                     bookRecordScanner = new Scanner(new FileInputStream(fileName));
+                } catch (FileNotFoundException e) {
+                    System.out.println("File not found. The program will now terminate");
+                    System.exit(0);
+                }
+
+
                     String line;
                     // Loop through each line in the current file
                     while ((line = fileReader.readLine()) != null) {
